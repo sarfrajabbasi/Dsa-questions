f1bb808615d178d4a89e592a388caa3f2e1b3b00
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-08-05 17:45:24 +0530

    dsa questions

--- JS/basicsProgramming.js
+++ JS/basicsProgramming.js
@@ -41,7 +41,8 @@ function feboNum(num) {
 
 feboNum(10);
 
-// count digit in a number
+// count digit in a number:--
+
 function countDigit(num) {
   let count = 0;
   while (num !== 0) {
@@ -54,4 +55,51 @@ function countDigit(num) {
 
 // countDigit(300)
 
-// 
+
+// Digits of a number:---
+
+function digitOfNum(num){
+  let nod = 0;
+  let temp = num;
+  while(temp !==0){
+    temp = parseInt(temp/10)
+    nod++
+  }
+  let div = Math.pow(10,nod-1);
+  // jabtak divisor zero nahi hoga tab tak loop chalega 
+  while(div !== 0 ){
+    // quotent value
+    const q = parseInt(num/div);
+    console.log(q);
+
+    // reminder values
+    num = num % div
+    div = parseInt(div / 10)
+  }
+
+}
+digitOfNum(7600)
+
+console.log("next solution:----");
+
+// Reverse Number:---
+
+function reverseNum(num){
+  while(num > 0){
+    let dig = num % 10
+    num = parseInt(num /10)
+    console.log(dig);
+  }
+  
+}
+
+reverseNum(54321)
+
+// Inverse of a number:--
+// --> The ineverse of the number is defined as the number created by interchanging the face value and index of digit from 1 to 5.
+// valid num:-- 624135,81456273,426135 etc
+// invaild num:-- 139,7421357.
+
+function inverseOfNum(){
+
+}
